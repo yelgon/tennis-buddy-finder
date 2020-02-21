e820f119f90f2c1b24ab2d6d04d67564b4bdc20c
Kim <kim@mr-roller.local>
2020-02-21 09:20:01 -0500

    correct

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import SimpleMap from "./SimpleMap.jsx";
 import SocialLogin from "./SocialLogin.jsx";
-import InputAddress from "./PlacesAuto.jsx";
+import PlacesAuto from "./PlacesAuto.jsx";
 
 class App extends Component {
   render = () => {
     return (
       <div>
         <div>
-          <InputAddress />
+          <PlacesAuto />
         </div>
         <div>
           <SocialLogin />
