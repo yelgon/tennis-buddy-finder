5a6333a52f267690bc3795c20f01e026757c829d
Kim <kim@mr-roller.local>
2020-02-21 13:49:26 -0500

    sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -12,13 +12,24 @@ const SideMenubar = styled.div`
     font-size: 15px;
     color: white;
     box-sizing: border-box;
-    margin: 0;
+    border-top: 1px solid rgba(255, 255, 255, 0.1);
+    border-bottom: 1px solid black;
+    margin-top: 0;
     padding: 0;
     list-style: none;
     text-decoration: none;
   }
   .fas {
     margin-right: 10px;
+    margin-left: 20px;
+  }
+  header {
+    font-size: 22px;
+    color: white;
+    text-align: center;
+    line-height: 60px;
+    background: #063146;
+    user-select: none;
   }
   position: fixed;
   left: 0;
@@ -30,68 +41,83 @@ const SideMenubar = styled.div`
 class Sidebar extends Component {
   render() {
     return (
-      <SideMenubar>
-        <ul>
-          <li>
-            <Link to="/home" style={{ color: "#FFF", textDecoration: "none" }}>
-              <i className="fas fa-home"></i> HOME
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" style={{ color: "#FFF", textDecoration: "none" }}>
-              <i className="fas fa-highlighter"></i> LOGIN
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/signup"
-              style={{ color: "#FFF", textDecoration: "none" }}
-            >
-              <i className="fas fa-pen"></i> SIGNUP
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/matchBoard"
-              style={{ color: "#FFF", textDecoration: "none" }}
-            >
-              <i className="fas fa-file"></i> MATCH BOARD
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/postPlayer"
-              style={{ color: "#FFF", textDecoration: "none" }}
-            >
-              <i className="fas fa-link"></i> POST PLAYER
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/postCourt"
-              style={{ color: "#FFF", textDecoration: "none" }}
-            >
-              <i className="fas fa-edit"></i> POST COURT
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/levelGuide"
-              style={{ color: "#FFF", textDecoration: "none" }}
-            >
-              <i className="fas fa-glasses"></i> LEVEL GUIDE
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/yourReservation"
-              style={{ color: "#FFF", textDecoration: "none" }}
-            >
-              <i className="fas fa-calendar-check"></i> YOUR RESERVATION
-            </Link>
-          </li>
-        </ul>
-      </SideMenubar>
+      <div>
+        <input type="checkbox" className="checkbox" />
+        <label htmlFor="check">
+          <i className="fas fa-bars"></i>
+          <i className="fas fa-times"></i>
+        </label>
+
+        <SideMenubar>
+          <header>My App</header>
+          <ul style={{ margin: "0", padding: "0" }}>
+            <li>
+              <Link
+                to="/home"
+                style={{ color: "#FFF", textDecoration: "none" }}
+              >
+                <i className="fas fa-home"></i> HOME
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/login"
+                style={{ color: "#FFF", textDecoration: "none" }}
+              >
+                <i className="fas fa-highlighter"></i> LOGIN
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/signup"
+                style={{ color: "#FFF", textDecoration: "none" }}
+              >
+                <i className="fas fa-pen"></i> SIGNUP
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/matchBoard"
+                style={{ color: "#FFF", textDecoration: "none" }}
+              >
+                <i className="fas fa-file"></i> MATCH BOARD
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/postPlayer"
+                style={{ color: "#FFF", textDecoration: "none" }}
+              >
+                <i className="fas fa-link"></i> POST PLAYER
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/postCourt"
+                style={{ color: "#FFF", textDecoration: "none" }}
+              >
+                <i className="fas fa-edit"></i> POST COURT
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/levelGuide"
+                style={{ color: "#FFF", textDecoration: "none" }}
+              >
+                <i className="fas fa-glasses"></i> LEVEL GUIDE
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/yourReservation"
+                style={{ color: "#FFF", textDecoration: "none" }}
+              >
+                <i className="fas fa-calendar-check"></i> YOUR RESERVATION
+              </Link>
+            </li>
+          </ul>
+        </SideMenubar>
+      </div>
     );
   }
 }
