f6f6a5a99517dfd2bbc24ffe1bb99794ee970114
Kim <kim@mr-roller.local>
2020-03-02 13:21:20 -0500

    level guide done

--- src/LevelGuide.jsx
+++ src/LevelGuide.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import { Circle } from "rc-progress";
+import styled from "styled-components";
+
+const Opa = styled.div`
+  opacity: 1;
+`;
+const Opacity = () => {};
+
 class TennisLevel extends Component {
   constructor() {
     super();
@@ -11,11 +18,29 @@ class TennisLevel extends Component {
       color: "#3FC7FA"
     };
     this.changeStateBeginner = this.changeStateBeginner.bind(this);
+    this.changeStateBeginner2 = this.changeStateBeginner2.bind(this);
     this.changeStateIntermediate = this.changeStateIntermediate.bind(this);
+    this.changeStateIntermediate2 = this.changeStateIntermediate2.bind(this);
     this.changeStateAdvance = this.changeStateAdvance.bind(this);
+    this.changeStateAdvance2 = this.changeStateAdvance2.bind(this);
   }
 
   changeStateBeginner() {
+    const colorMap = ["#ccff13", "#6fdf87", "#4ccdd1", "#2f5acf", "#df5f2d"];
+    const forehandValue = parseInt(Math.random() * (30 - 10) + 10, 10);
+    const backhandValue = parseInt(Math.random() * (20 - 10) + 10, 10);
+    const serveValue = parseInt(Math.random() * (20 - 10) + 10, 10);
+    const volleyValue = parseInt(Math.random() * (20 - 10) + 10, 10);
+
+    this.setState({
+      forehandPercent: forehandValue,
+      backhandPercent: backhandValue,
+      servePercent: serveValue,
+      volleyPercent: volleyValue,
+      color: colorMap[parseInt(Math.random() * 5, 10)]
+    });
+  }
+  changeStateBeginner2() {
     const colorMap = ["#ccff13", "#6fdf87", "#4ccdd1", "#2f5acf", "#df5f2d"];
     const forehandValue = parseInt(Math.random() * (50 - 30) + 30, 10);
     const backhandValue = parseInt(Math.random() * (40 - 20) + 20, 10);
@@ -32,8 +57,23 @@ class TennisLevel extends Component {
   }
   changeStateIntermediate() {
     const colorMap = ["#9416dd", "#e06b27", "#4da8dd", "#9cc213", "#28caaf"];
-    const forehandValue = parseInt(Math.random() * (90 - 65) + 65, 10);
-    const backhandValue = parseInt(Math.random() * (80 - 60) + 60, 10);
+    const forehandValue = parseInt(Math.random() * (60 - 45) + 45, 10);
+    const backhandValue = parseInt(Math.random() * (60 - 50) + 50, 10);
+    const serveValue = parseInt(Math.random() * (55 - 50) + 50, 10);
+    const volleyValue = parseInt(Math.random() * (55 - 40) + 40, 10);
+
+    this.setState({
+      forehandPercent: forehandValue,
+      backhandPercent: backhandValue,
+      servePercent: serveValue,
+      volleyPercent: volleyValue,
+      color: colorMap[parseInt(Math.random() * 5, 10)]
+    });
+  }
+  changeStateIntermediate2() {
+    const colorMap = ["#9416dd", "#e06b27", "#4da8dd", "#9cc213", "#28caaf"];
+    const forehandValue = parseInt(Math.random() * (80 - 65) + 65, 10);
+    const backhandValue = parseInt(Math.random() * (75 - 60) + 60, 10);
     const serveValue = parseInt(Math.random() * (70 - 50) + 50, 10);
     const volleyValue = parseInt(Math.random() * (70 - 40) + 40, 10);
 
@@ -47,10 +87,25 @@ class TennisLevel extends Component {
   }
   changeStateAdvance() {
     const colorMap = ["#dd1691", "#00ff00", "#f70088", "#0172cf", "#ff0040"];
-    const forehandValue = parseInt(Math.random() * (100 - 80) + 80, 10);
-    const backhandValue = parseInt(Math.random() * (100 - 80) + 80, 10);
-    const serveValue = parseInt(Math.random() * (100 - 80) + 80, 10);
-    const volleyValue = parseInt(Math.random() * (100 - 80) + 80, 10);
+    const forehandValue = parseInt(Math.random() * (90 - 80) + 80, 10);
+    const backhandValue = parseInt(Math.random() * (90 - 80) + 80, 10);
+    const serveValue = parseInt(Math.random() * (90 - 80) + 80, 10);
+    const volleyValue = parseInt(Math.random() * (90 - 80) + 80, 10);
+
+    this.setState({
+      forehandPercent: forehandValue,
+      backhandPercent: backhandValue,
+      servePercent: serveValue,
+      volleyPercent: volleyValue,
+      color: colorMap[parseInt(Math.random() * 5, 10)]
+    });
+  }
+  changeStateAdvance2() {
+    const colorMap = ["#dd1691", "#00ff00", "#f70088", "#0172cf", "#ff0040"];
+    const forehandValue = parseInt(Math.random() * (100 - 95) + 95, 10);
+    const backhandValue = parseInt(Math.random() * (100 - 95) + 95, 10);
+    const serveValue = parseInt(Math.random() * (100 - 95) + 95, 10);
+    const volleyValue = parseInt(Math.random() * (100 - 95) + 95, 10);
 
     this.setState({
       forehandPercent: forehandValue,
@@ -70,8 +125,8 @@ class TennisLevel extends Component {
     } = this.state;
 
     const circleContainerStyle = {
-      width: "200px",
-      height: "200px",
+      width: "170px",
+      height: "170px",
       display: "inline-block",
       textAlign: "center",
       marginBottom: "85px",
@@ -82,13 +137,22 @@ class TennisLevel extends Component {
       <div className="tennisLevelWrapper">
         <div className="level">
           <button tydive="button" onClick={this.changeStateBeginner}>
-            BEGINNER <br></br>(2.0-3.0)
+            2.0
+          </button>
+          <button tydive="button" onClick={this.changeStateBeginner2}>
+            2.5
           </button>
           <button tydive="button" onClick={this.changeStateIntermediate}>
-            INTERMEDIATE <br></br>(3.0-4.0)
+            3.0
+          </button>
+          <button tydive="button" onClick={this.changeStateIntermediate2}>
+            3.5
           </button>
           <button tydive="button" onClick={this.changeStateAdvance}>
-            ADVANCE <br></br>(4.0-4.5)
+            4.0
+          </button>
+          <button tydive="button" onClick={this.changeStateAdvance2}>
+            4.5
           </button>
         </div>
 
